81eafa2f8cbe5b4a99c2ec4ea4209e36d589dc21
Tigo-Developers <OperacionesDigitales@amstigo.com.co>
2022-10-27 16:55:28 -0500

    adición de contenido de amazon prime

--- js/main.js
+++ js/main.js
@@ -1,43 +1,46 @@
-$(document).ready(function(){
-    //Aqui tu codigo
-    
+$(document).ready(function () {
+  //Aqui tu codigo
+
   //  $("#paginas").load('archivos/'+ 'inicio' +'.html');
 
+  inicilizarComponentes();
+
+
+  $('#menu').slicknav({
+    label: 'Menú'
+  });
+
+
+
+
+  $(".click").click(function () {
+    console.log($(this).attr("value"));
+    $("#paginas").empty().html();
+    let documento = $(this).attr("value");
+
+    if (documento) {
+      $("#video").hide();
+    }
+
+
+
+
+    $("#paginas").load('archivos/' + documento + '.html');
     inicilizarComponentes();
- 
-   
-    $('#menu').slicknav({ 
-            label: 'Menú'        
+  });
+
+  function inicilizarComponentes() {
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+    $('.slider').slider({
+      indicators: true
     });
+    $('.tabs').tabs();
 
-     
-    
-    $(".click").click(function(){
-        console.log($(this).attr("value"));
-        $("#paginas").empty().html();        
-        let documento = $(this).attr("value");
-
-     
+  }
 
 
-        $("#paginas").load('archivos/'+ documento +'.html');
-        inicilizarComponentes();
-      });
 
-      function inicilizarComponentes() {
-        $(".dropdown-trigger").dropdown();
-        $('.sidenav').sidenav();
-        $('.slider').slider({
-          indicators: true
-        });
-        $('.tabs').tabs();
-        
 
-        console.log("componrnyrd");
-      }
 
-     
-
-      
-      
 });
